376b259233af7937e47dfc3da260dd4ab47128be
alexjosesp <alex.jose.suca@gmail.com>
2018-05-29 01:20:07 -0500

    Initial commit

---
+++ application/views/view_comensales.php
@@ -0,0 +1,79 @@
+
+<script type="text/javascript">
+            /*CLIENTES*/
+            $(document).ready(function() {
+                $('#comensales').dataTable( {
+                    // sDom: hace un espacio entre la tabla y los controles 
+                "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+        
+                } );
+            } );
+</script>
+
+<div id="container">
+	<h2 align="center">CATALOGO DE ESTUDIANTES</h2>
+	<?php
+if(isset($_GET['save'])){
+	echo '<div class="alert alert-success text-center">La Información  se Almaceno Correctamente</div>';
+}
+if(isset($_GET['delete'])){
+	echo '<div class="alert alert-warning text-center">La Información  se ha Eliminado Correctamente</div>';
+}
+if(isset($_GET['update'])){
+	echo '<div class="alert alert-success text-center">La Información  se Actualizo Correctamente</div>';
+}
+if(isset($_GET['permisos'])){
+		echo '<div class="alert alert-success text-center">Los Permisos fueron Asignados Correctamente</div>';
+	}
+	if(isset($_GET['password'])){
+		echo '<div class="alert alert-success text-center">La Contraseña fue actualizado Correctamente</div>';
+	}
+?>
+<center>
+<table id="comensales" border="0" cellpadding="0" cellspacing="0" class="pretty">
+<thead>
+<tr>
+<th>ACCION</th>
+<th>CODIGO</th>
+<th>NOMBRE</th>
+<th>APELLIDOS</th>
+<th>SEXO</th>
+<th>DNI</th>
+<th>FACULTAD</th>
+<th>EDAD</th>
+<th>TURNO</th>
+<th>CORREO</th>
+</tr>
+</thead>
+<tbody>
+ <?php 
+ $contador = 0;
+ if(!empty($comensales)){
+ 	foreach($comensales as $comensal){
+ 		echo '<tr>';
+		echo '<td>'
+?>
+		<a href="<?php echo base_url();?>index.php/comensales/editar/<?php echo $comensal->CODIGO;?>/" class="btn btn-success">Editar</a>
+		<a href="<?php echo base_url();?>index.php/comensales/password/<?php echo $comensal->CODIGO ?>" class="btn btn-default">Password</a>
+		<a href="<?php echo base_url();?>index.php/comensales/permisos/<?php echo $comensal->CODIGO;?>" class="btn btn-info">Permisos</a>
+		<a href="<?php echo base_url();?>index.php/comensales/eliminar/<?php echo $comensal->CODIGO ?>" class="btn btn-danger">Eliminar</a>
+<?php		
+		echo '</td>';
+		echo '<td>'.$comensal->CODIGO.'</td>';
+		echo '<td>'.$comensal->NOMBRE.'</td>';
+ 		echo '<td>'.$comensal->APELLIDOS.'</td>';
+		echo '<td>'.$comensal->SEXO.'</td>';
+		echo '<td>'.$comensal->DNI.'</td>';
+		echo '<td>'.$comensal->FACULTAD.'</td>';
+		echo '<td>'.$comensal->EDAD.'</td>';
+		echo '<td>'.$comensal->TURNO.'</td>';
+		echo '<td>'.$comensal->CORREO.'</td>';
+ 		echo '</tr>';
+ 	} 
+ }
+
+ ?>
+</tbody>
+</table>
+</center>
+</div>
---
+++ application/views/view_nuevo_comensal.php
@@ -0,0 +1,167 @@
+<?php
+	  echo '<center>';
+	  echo '<table border=0 class="ventanas" width="650" cellspacing="0" cellpadding="0">';
+	  echo '<tr>';
+	  echo "<td height='10' class='tabla_ventanas_login' height='10' colspan='3'><legend align='center'>NUEVO ESTUDIANTE</legend></td>";
+	  echo '</tr>';
+	  echo '<tr><td colspan=3>';
+	  $attributes = array("class" => "form-horizontal", "id" => "form", "name" => "form");
+	  //echo form_open("clientes/Save", $attributes);
+	  echo form_open();
+	  echo '<center>';
+	  echo '<table border=0>';
+	  
+	#dibujamos campos texto
+	$Codigo 	  = array(
+	'name'        => 'CODIGO',
+	'id'          => 'CODIGO',
+	'size'        => 11,
+	'value'		  => set_value('CODIGO',@$datos_comensales[0]->CODIGO),
+	'placeholder' => 'Codigo',
+	'type'        => 'text',
+	);
+	echo '<tr>';
+	echo '<td>'.form_label("Codigo:",'CODIGO').'</td>';
+	echo '<td>';
+	echo form_input($Codigo);
+	echo '</td>';
+	echo '<td><font color="red">'.form_error('CODIGO').'</font></td>';
+	echo '</tr>';
+
+	$Nombre 	  = array(
+	'name'        => 'NOMBRE',
+	'id'          => 'NOMBRE',
+	'size'        => 50,
+	'value'		  => set_value('NOMBRE',@$datos_comensales[0]->NOMBRE),
+	'placeholder' => 'Nombre',
+	'type'        => 'text',
+	);
+	echo '<tr>';
+	echo '<td>'.form_label("Nombre:",'NOMBRE').'</td>';
+	echo '<td>';
+	echo form_input($Nombre);
+	echo '</td>';
+	echo '<td><font color="red">'.form_error('NOMBRE').'</font></td>';
+	echo '</tr>';
+	
+	$Apellidos = array(
+	'name'        => 'APELLIDOS',
+	'id'          => 'APELLIDOS',
+	'size'        => 100,
+	'value'		  => set_value('APELLIDOS',@$datos_comensales[0]->APELLIDOS),
+	'placeholder' => 'Apellidos',
+	'type'        => 'text',
+	);
+	echo '<tr>';
+	echo '<td>'.form_label("Apellidos:",'APELLIDOS').'</td>';
+	echo '<td>';
+	echo form_input($Apellidos);
+	echo '</td>';
+	echo '<td><font color="red">'.form_error('APELLIDOS').'</font></td>';
+	echo '</tr>';
+	
+
+	$Sexo = array(
+	'0'               	=> 'SELECCIONE EL SEXO DEL ESTUDIANTE',
+	'Masculino'		=> 'Masculino',
+	'Femenino'	    	=> 'Femenino',
+	);
+	echo '<tr>';
+    echo '<td>'.form_label("Sexo:",'SEXO').'</td>';
+    echo '<td>';
+    echo  form_dropdown('SEXO', $Sexo, set_value('SEXO',@$datos_comensales[0]->SEXO));
+    echo '</td>';
+    echo '<td><font color="red">'.form_error('SEXO').'</font></td>';
+    echo '</tr>';
+
+	$Dni 		  = array(
+	'name'        => 'DNI',
+	'id'          => 'DNI',
+	'size'        => 50,
+	'value'		  => set_value('DNI',@$datos_comensales[0]->DNI),
+	'placeholder' => 'Dni',
+	'type'        => 'text',
+	);
+	echo '<tr>';
+	echo '<td>'.form_label("DNI:",'DNI').'</td>';
+	echo '<td>';
+	echo form_input($Dni);
+	echo '</td>';
+	echo '<td><font color="red">'.form_error('DNI').'</font></td>';
+	echo '</tr>';
+
+	$Facultad 		  = array(
+	'name'        => 'FACULTAD',
+	'id'          => 'FACULTAD',
+	'size'        => 50,
+	'value'		  => set_value('FACULTAD',@$datos_comensales[0]->FACULTAD),
+	'placeholder' => 'Facultad',
+	'type'        => 'text',
+	);
+	echo '<tr>';
+	echo '<td>'.form_label("FACULTAD:",'FACULTAD').'</td>';
+	echo '<td>';
+	echo form_input($Facultad);
+	echo '</td>';
+	echo '<td><font color="red">'.form_error('FACULTAD').'</font></td>';
+	echo '</tr>';
+
+	$Edad 		  = array(
+	'name'        => 'EDAD',
+	'id'          => 'EDAD',
+	'size'        => 50,
+	'value'		  => set_value('EDAD',@$datos_comensales[0]->EDAD),
+	'placeholder' => 'Edad',
+	'type'        => 'text',
+	);
+	echo '<tr>';
+	echo '<td>'.form_label("EDAD:",'EDAD').'</td>';
+	echo '<td>';
+	echo form_input($Edad);
+	echo '</td>';
+	echo '<td><font color="red">'.form_error('EDAD').'</font></td>';
+	echo '</tr>';
+	
+	$Turno = array(
+	'0'               	=> 'SELECCIONE EL TURNO',
+	'Mañana'		=> 'Mañana',
+	'Tarde'	    	=> 'Tarde',
+	);
+	echo '<tr>';
+    echo '<td>'.form_label("Turno:",'TURNO').'</td>';
+    echo '<td>';
+    echo  form_dropdown('TURNO', $Turno, set_value('TURNO',@$datos_comensales[0]->TURNO));
+    echo '</td>';
+    echo '<td><font color="red">'.form_error('TURNO').'</font></td>';
+    echo '</tr>';
+
+    $Correo 		  = array(
+	'name'        => 'CORREO',
+	'id'          => 'CORREO',
+	'size'        => 50,
+	'value'		  => set_value('CORREO',@$datos_comensales[0]->CORREO),
+	'placeholder' => 'Correo',
+	'type'        => 'text',
+	);
+	echo '<tr>';
+	echo '<td>'.form_label("CORREO:",'CORREO').'</td>';
+	echo '<td>';
+	echo form_input($Correo);
+	echo '</td>';
+	echo '<td><font color="red">'.form_error('CORREO').'</font></td>';
+	echo '</tr>';  
+		
+	echo '<tr>';
+	echo '<td colspan=3>'.$this->session->flashdata('msg').'</td>';
+	echo '</tr>';
+	echo '<tr><td colspan=3><hr/></td></tr>';
+	echo '<tr>';
+	echo '<td colspan=3><center>';
+	echo '<input type="submit" class="btn btn-success" value="Guardar">';
+    echo '</center></td></tr>';
+    echo '</table></center>';
+    echo form_close(); 
+    echo '</td></tr>';
+    echo '</table>';
+    echo '</center>';
+?>
