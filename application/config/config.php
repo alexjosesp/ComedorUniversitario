e6c9ee6f5dfae99a76ae4e481eb4e68fb8d2b5d8
alexjosesp <alex.jose.suca@gmail.com>
2018-06-02 12:25:56 -0500

    CONFIGURACION PATH
    CONFIGURACION DEL URL BASE, POR MOTIVOS DE PRUEBAS

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ComedorUniversitario/';
+$config['base_url']	= 'http://172.16.0.235:8080/ComedorUniversitario/';
 
 /*
 |--------------------------------------------------------------------------
